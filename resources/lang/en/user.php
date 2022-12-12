84cb2e0937279e5c4e7da9fa6c8de27931bfee69
mimii97 <marina.alhassan97@gmail.com>
2022-12-12 13:42:14 +0200

    trans

--- resources/lang/en/user.php
+++ resources/lang/en/user.php
@@ -12,8 +12,69 @@ return [
 	 */
 	'logout'=>'logout',
 	'home'=>'home',
+	'show'=>'show',
 	'dashboard'=>'dashboard',
 	'comments'=>'comments',
 	'reactions'=>'reactions',
-	'icousers'=>'ICO'
+	'icousers'=>'ICO',
+	'add'=>'add',
+	'num_likes'=>'likes',
+	'num_dislikes'=>'dislikes',
+	'num_comments'=>'comments',
+	'kuro_balance'=>'Balance',
+	'user_vote_revenue'=>'Vote revenue',
+	'paid_vote_plan_balance'=>'Paide vote plane Balance',
+	'bfotplans'=>'Be from our team Plans',
+	'requests'=>'requests',
+	'create'=>'create',
+	'added'=>'added',
+	'icos'=>'icos',
+	'reactions'=>'reactions',
+	'error_permission_1'=>'Error',
+	'error_permission_2'=>'Error',
+	'error_permission_3'=>'Error',
+	'error_permission_4'=>'Error',
+	'error_permission_5'=>'Error',
+	'error_permission_6'=>'Error',
+	'reset_password'=>'Reset password',
+	'alert'=>'Alert',
+	'success'=>'Success',
+	'email'=>'Email',
+	'password'=>'Password',
+	'password_confirmation'=>'Password Confirmation',
+	'andlogin'=>'remember me',
+	'change_password'=>'change password',
+	'login'=>'Login',
+	'en'=>'English',
+	'ar'=>'Arabic',
+	'show_all'=>'show all',
+	'subject'=>'Subject',
+	'message'=>'Message',
+	'add_back'=>'Submet',
+	'id'=>'id',
+	'name'=>'name',
+	'email'=>'email',
+	'subject'=>'subject',
+	'message'=>'message',
+	'voteplans'=>'vote plans',
+	'bfotplans'=>'bfot plans',
+	'icousers'=>'ICO Users',
+	'comments'=>'Comments',
+	'ask_del'=>'Delete',
+	'approval'=>'Approval',
+	'cancel'=>'Cancle',
+	'type'=>'type',
+	'description'=>'description',
+	'num_votes_cond'=>'Number of votes',
+	'kuro_balance_cond'=>'Balance',
+	
+
+
+
+
+
+
+
+
+
 ];
